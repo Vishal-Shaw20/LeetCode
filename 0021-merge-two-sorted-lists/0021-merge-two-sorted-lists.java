d452d5f2f562d776d0ced3f3355da4e38b9df859
Vishal Shaw <vishalshawi123@gmail.com>
2025-10-28 09:58:45 +0530

    Time: 0 ms (100%), Space: 42.8 MB (24.74%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -9,36 +9,42 @@
  * }
  */
 class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2)
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) 
     {
-        if(list1 == null || list2 == null)
-            return ((list1 == null)? list2 : list1);
-        if(list1.val <= list2.val)
-            return merge(list1,list2);
-        else
-            return merge(list2,list1);
-    }
-
-    public ListNode merge(ListNode list1, ListNode list2) {
-        if(list2 == null)
-            return list1;
         if(list1 == null)
             return list2;
-        if(list1.next == null)
-        {
-            list1.next = merge(list1.next, list2);
+        else if(list2 == null)
             return list1;
-        }
-        if(list1.val <= list2.val && list1.next.val >= list2.val)
+        ListNode list3 = null, temp = null;
+        while(list1 != null && list2 != null)
         {
-            ListNode temp = list2;
-            list2 = list2.next;
-            temp.next = list1.next;
-            list1.next = temp;
-            list1.next = merge(list1.next,list2);
+            if(list1.val <= list2.val)
+            {
+                if(list3 == null)
+                    list3 = temp = list1;
+                else
+                {
+                    temp.next = list1;
+                    temp = temp.next;
+                }
+                list1 = list1.next;
+            }
+            else
+            {
+                if(list3 == null)
+                    list3 = temp = list2;
+                else
+                {
+                    temp.next = list2;
+                    temp = temp.next;
+                }
+                list2 = list2.next;
+            }
         }
-        else
-            list1.next = merge(list1.next,list2);
-        return list1;
+        if(list1 != null)
+            temp.next = list1;
+        else 
+            temp.next = list2;
+        return list3;
     }
 }
