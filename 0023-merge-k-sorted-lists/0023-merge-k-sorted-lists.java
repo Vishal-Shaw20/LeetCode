cb09df0a095f1396e33cad24e82b2d125603ab12
Vishal Shaw <vishalshawi123@gmail.com>
2025-10-29 18:33:22 +0530

    Time: 4 ms (76.24%), Space: 44.8 MB (29.19%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -9,59 +9,25 @@
  * }
  */
 class Solution {
-    public static ListNode mergeKLists(ListNode[] lists) {
+    public ListNode mergeKLists(ListNode[] lists) 
+    {
         if(lists.length == 0)
             return null;
-        ListNode[] ptr = lists;
-        ListNode root, temp;
-        int min = 0, i = 0, k = 0, n = lists.length;
-        boolean T = true;
-        for(int j = 0; j < n; j++)
+        ListNode dummyNode = new ListNode(-1);
+        ListNode temp = dummyNode;
+        PriorityQueue<ListNode> minheap = new PriorityQueue<>((n1,n2) -> n1.val - n2.val);
+        for(int i = 0; i < lists.length; i++)
         {
-            if(lists[j] != null)
-            {
-                min = lists[j].val;
-                break;
-            }
-            if(j == (n - 1))
-                return null;
+            if(lists[i] != null)
+                minheap.add(lists[i]);
         }
-        root = temp = new ListNode(min);
-        while(true)
+        while(!minheap.isEmpty())
         {
-            for(int m = 0; m < n; m++)
-            {
-                if(ptr[m] != null)
-                {
-                    min = lists[m].val;
-                    k = m;
-                    break;
-                }
-                if(m == (n - 1))
-                    return root;
-            }
-            for(int m = 0; m < n; m++)
-            {
-                if (ptr[m] == null)
-                    continue;
-                if (ptr[m].val < min)
-                {
-                    k = m;
-                    min = ptr[m].val;
-                }
-            }
-            if(T)
-            {
-                root.val = ptr[k].val;
-                ptr[k] = ptr[k].next;
-                T = false;
-            }
-            else
-            {
-                temp.next = new ListNode(ptr[k].val);
-                temp = temp.next;
-                ptr[k] = ptr[k].next;
-            }
+            temp.next = minheap.poll();
+            temp = temp.next;
+            if(temp.next != null)
+                minheap.add(temp.next);
         }
+        return dummyNode.next;
     }
 }
