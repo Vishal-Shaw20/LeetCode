c12663c78957858ce49ff0337b2cd16c34c4fa74
Vishal Shaw <vishalshawi123@gmail.com>
2026-09-04 15:15:16 +0530

    Time: 2 ms (89.02%), Space: 48 MB (23.6%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxIndex = 0;
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(i > maxIndex)
+                return false;
+            if((i + nums[i]) > maxIndex)
+                maxIndex = i + nums[i];
+        }
+        return true;
+    }
+}
